dd931d2386aa280ea0464b7682f862f55aa4f0db
Adharsh Rajendran <adharsh.rajendran@ibm.com>
2023-09-07 23:29:59 -0700

    First runthrough of program

---
+++ Day1/band-name-generator.py
@@ -0,0 +1,6 @@
+# Day 1 of Python Programming
+# This task is serving as a practice and refresher.
+print("Welcome to the Band Name")
+city = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your Pets name?\n")
+print("Your band name could be: " + city + " " + pet_name)
