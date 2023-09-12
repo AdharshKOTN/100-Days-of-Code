aee252b313243a45209a87bd3d67f4c0f4963837
Adharsh Rajendran <adharsh.rajendran@ibm.com>
2023-09-12 00:22:00 -0500

    fixed error in f string

--- Day2/tip-calculator.py
+++ Day2/tip-calculator.py
@@ -3,4 +3,4 @@ total_bill = float(input("What was the total bill? $"))
 tip_percent = (float(input("What percentage tip would you like to give? 10, 12 or 15? ")) / 100 ) + 1
 party_size = float(input("How many people split the bill? "))
 individual_payment = round(total_bill * tip_percent / ( party_size if party_size > 1 else 1 ), 2)
-print(f"Each person should pay: $" + str(individual_payment))
+print(f"Each person should pay: ${individual_payment}"))
