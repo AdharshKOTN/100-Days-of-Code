fe2c26a39069e736fec8dc73c116f6740d7e82d2
Adharsh Rajendran <adharsh.rajendran@ibm.com>
2023-09-12 00:19:32 -0500

    fixed logical error in tip percentage

--- Day2/tip-calculator.py
+++ Day2/tip-calculator.py
@@ -1,6 +1,6 @@
 print("Welcome to the tip calculator.")
 total_bill = float(input("What was the total bill? $"))
+tip_percent = (float(input("What percentage tip would you like to give? 10, 12 or 15? ")) / 100 ) + 1
 party_size = float(input("How many people split the bill? "))
-tip_percent = float(input("What percentage tip would you like to give? 10, 12 or 15? "))
 individual_payment = round(total_bill * tip_percent / ( party_size if party_size > 1 else 1 ), 2)
-print("Each person should pay: $" + str(individual_payment))
+print(f"Each person should pay: $" + str(individual_payment))
